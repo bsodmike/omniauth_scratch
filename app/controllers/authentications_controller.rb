204ed7fffe54c99b643767957273cf6e909979b6
Michael de Silva <michael@mwdesilva.com>
2011-06-19 19:14:57 +0300

    updated authentications#create

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,7 +4,10 @@ class AuthenticationsController < ApplicationController
   end
 
   def create
-    render :text => request.env['omniauth.auth'].to_yaml
+    auth = request.env['omniauth.auth']
+    current_user.authentications.create(:provider => auth['provider'], :uid => auth['uid'])
+    flash[:notice] = "Authentication successful"
+    redirect_to authentications_url
   end
 
   def destroy
