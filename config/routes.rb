7a4e22ab5acebfc82bf9b241252885c74cecc08e
Michael de Silva <michael@mwdesilva.com>
2011-06-19 11:43:21 +0300

    added route for logging out and destroy action in sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Auth::Application.routes.draw do
+  get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
   root :to => "users#new"
