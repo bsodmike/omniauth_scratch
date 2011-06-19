c4c7d2c71ce7cb20e694f3202fc4b6cc067d8a1a
Michael de Silva <michael@mwdesilva.com>
2011-06-19 19:09:09 +0300

    added route for omniauth response (callback) and simple test in controller to render authentication details as YAML

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,12 +4,7 @@ class AuthenticationsController < ApplicationController
   end
 
   def create
-    @authentication = Authentication.new(params[:authentication])
-    if @authentication.save
-      redirect_to authentications_url, :notice => "Successfully created authentication."
-    else
-      render :action => 'new'
-    end
+    render :text => request.env['omniauth.auth'].to_yaml
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Auth::Application.routes.draw do
-  resources :authentications
-
+  match '/auth/:provider/callback' => 'authentications#create'
   match "/log_out" => "sessions#destroy", :as => "log_out"
   match "/log_in" => "sessions#new", :as => "log_in"
   match "/sign_up" => "users#new", :as => "sign_up"
   root :to => "users#new"
   resources :users
   resources :sessions
+  resources :authentications
 end
